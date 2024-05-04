f11387461e105da7cee6b6f5a31aa09a08bfba1c
UltWolf <UltWolf@DESKTOP-CRA00F5>
2024-05-04 13:39:26 +0300

    Add project files.

---
+++ Daily/Day542024/BoatsSavePeople.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.Daily.Day542024
+{
+    public class BoatsSavePeople
+    {
+        //my solution
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            var list = people.ToList();
+            list.Sort((a, b) => { return b.CompareTo(a); });
+            int numb = 0;
+            for (var i = 0; i < list.Count(); i++)
+            {
+
+                if (list[i] == limit)
+                {
+                    numb++;
+                    continue;
+                }
+                for (var j = list.Count - 1; j > i; j--)
+                {
+                    if (list[i] + list[j] <= limit)
+                    {
+                        list.Remove(list[j]);
+                        break;
+                    }
+                }
+                numb++;
+            }
+            return numb;
+        }
+        //optimized solution
+        public int NumRescueBoats2(int[] people, int limit)
+        {
+
+            Array.Sort(people);
+            int i = 0;
+            int j = people.Length - 1;
+            int ans = 0;
+            while (i <= j)
+            {
+                if (people[i] + people[j] <= limit)
+                    i++;
+
+                j--;
+                ans++;
+            }
+            return ans;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using LeetCode.Daily.Day542024;
+
+Console.WriteLine(new BoatsSavePeople().NumRescueBoats(new int[] { 3, 5, 3, 4 }, 5));
+Console.WriteLine(new BoatsSavePeople().NumRescueBoats(new int[] { 1, 2 }, 3));

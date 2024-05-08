7ca15d5e335132355f711d69395c207969d39472
ultwolf <ultwolf@gmail.com>
2024-05-08 12:23:47 +0300

    add solution for 5/8/2024

---
+++ LeetCode.Test/Daily/Day582024/RanksTest.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Daily.Day582024;
+using System.Collections;
+
+namespace LeetCode.Test.Daily.Day582024
+{
+    public class RanksTest
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new int[] { 5, 4, 3, 2, 1 },
+                    new string[] { "Gold Medal", "Silver Medal", "Bronze Medal", "4", "5" }
+                    );
+                yield return new TestCaseData(
+                    new int[] { 10, 3, 8, 9, 4 },
+                    new string[] { "Gold Medal", "5", "Bronze Medal", "Silver Medal", "4" }
+                    );
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TestRanked(int[] array, string[] expectedArray)
+        {
+            Assert.AreEqual(new Ranks().FindRelativeRanks(array), expectedArray);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day582024/Ranks.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Daily.Day582024
+{
+    public class Ranks
+    {
+        public string[] FindRelativeRanks(int[] score)
+        {
+            var rank = new string[score.Length];
+            var originalScore = (int[])score.Clone();
+            Array.Sort(score,
+                new Comparison<int>(
+                (c, p) => { return p.CompareTo(c); }
+                )
+                );
+            for (var i = 0; i < originalScore.Length; i++)
+            {
+                for (var j = 0; j < score.Length; j++)
+                {
+                    if (originalScore[i] == score[j])
+                    {
+
+                        if (j == 0)
+                        {
+                            rank[i] = "Gold Medal";
+                        }
+                        else if (j == 1)
+                        {
+                            rank[i] = "Silver Medal";
+                        }
+                        else if (j == 2)
+                        {
+                            rank[i] = "Bronze Medal";
+                        }
+                        else
+                        {
+                            rank[i] = (j + 1).ToString();
+                        }
+                        break;
+                    }
+                }
+            }
+            return rank;
+        }
+    }
+}

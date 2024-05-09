5f0c179cd31cbd7664a54e831094075d28e8c45e
Vitaliy Zayarniy <ultwolf@gmail.com>
2024-05-09 13:07:23 +0300

    push some changes

---
+++ LeetCode.Test/Daily/Day592024/ChildHappinessTest.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Test.Daily.Day592024
+{
+    public class ChildHappinessTest
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new int[] { 1, 2, 3 },
+                    2,
+                    4
+                    );
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TestRanked(int[] array, int k, int actualResult)
+        {
+            Assert.AreEqual(new ChildHappiness().(array), expectedArray);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day592024/ChildHappiness.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Daily.Day592024
+{
+    public class ChildHappiness
+    {
+        public long MaximumHappinessSum(int[] happiness, int k)
+        {
+            Array.Sort(happiness);
+            long result = 0;
+            long diff = 0;
+            for (var i = 0; i < happiness.Length && k != 0; i++, k--, diff++)
+            {
+                var addValue = happiness[i] - diff;
+                if (addValue < 0)
+                {
+                    addValue = 0;
+                }
+
+                result += addValue;
+
+            }
+            return result;
+        }
+    }
+}

daef88cbf56000a18a45f6dafc217f7d022d7e69
ultwolf <ultwolf@gmail.com>
2024-05-14 13:12:57 +0300

    solution

---
+++ LeetCode.Test/Daily/Day5142024/MaxGoldTest.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Daily.Day5142024;
+using System.Collections;
+
+namespace LeetCode.Test.Daily.Day5142024
+{
+    public class MaxGoldTest
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new int[][] { [0, 6, 0], [5, 8, 7], [0, 9, 0] },
+                     24
+                    );
+                yield return new TestCaseData(
+             new int[][] { [1, 0, 7, 0, 0, 0], [2, 0, 6, 0, 1, 0], [3, 5, 6, 7, 4, 2], [4, 3, 1, 0, 2, 0], [3, 0, 5, 0, 20, 0] },
+                60
+               );
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TestGold(int[][] array, int expectedResult)
+        {
+            Assert.AreEqual(new MaximumGold().GetMaximumGold(array), expectedResult);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day5142024/MaximumGold.cs
@@ -0,0 +1,76 @@
+﻿namespace LeetCode.Daily.Day5142024
+{
+    public class MaximumGold
+    {
+        public int GetMaximumGold(int[][] grid)
+        {
+
+            if (grid == null || grid.Length == 0)
+                return 0;
+
+            int m = grid.Length, n = grid[0].Length, max = 0; ;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    max = Math.Max(max, Move(grid, i, j, new bool[m, n]));
+                }
+            }
+
+            return max;
+        }
+
+        private int Move(int[][] grid, int i, int j, bool[,] visited)
+        {
+            int m = grid.Length, n = grid[0].Length;
+            if (i < 0 || i >= m || j < 0 || j >= n || visited[i, j] || grid[i][j] == 0)
+                return 0;
+            visited[i, j] = true;
+            int up = Move(grid, i - 1, j, visited);
+            int down = Move(grid, i + 1, j, visited);
+            int left = Move(grid, i, j - 1, visited);
+            int right = Move(grid, i, j + 1, visited);
+            int sum = Math.Max(Math.Max(up, down), Math.Max(left, right)) + grid[i][j];
+            visited[i, j] = false;
+            return sum;
+        }
+        //public int Move(int[][] grid, int index, int endIndex, int result)
+        //{
+
+        //    (int, int)? largeIndex = null;
+        //    int acc = 0;
+        //    int leftIndex = endIndex - 1;
+        //    if (leftIndex >= 0 && grid[index][leftIndex] > acc && !visitedIndexes.Any(f => f.Item1 == index && f.Item2 == leftIndex))
+        //    {
+        //        acc = grid[index][leftIndex];
+        //        largeIndex = new(index, leftIndex);
+        //    }
+        //    int upperIndex = index - 1;
+        //    if (upperIndex >= 0 && grid[upperIndex][endIndex] > acc && !visitedIndexes.Any(f => f.Item1 == upperIndex && f.Item2 == endIndex))
+        //    {
+        //        acc = grid[upperIndex][endIndex];
+        //        largeIndex = new(upperIndex, endIndex);
+        //    }
+        //    int lowerIndex = index + 1;
+        //    if (lowerIndex < grid.Length && grid[lowerIndex][endIndex] > acc && !visitedIndexes.Any(f => f.Item1 == lowerIndex && f.Item2 == endIndex))
+        //    {
+        //        acc = grid[lowerIndex][endIndex];
+        //        largeIndex = new(lowerIndex, endIndex);
+        //    }
+        //    int rightIndex = endIndex + 1;
+        //    if (rightIndex < grid[index].Length && grid[index][rightIndex] > acc && !visitedIndexes.Any(f => f.Item1 == index && f.Item2 == rightIndex))
+        //    {
+        //        acc = grid[index][rightIndex];
+        //        largeIndex = new(index, rightIndex);
+        //    }
+
+        //    if (largeIndex != null)
+        //    {
+        //        result += acc;
+        //        visitedIndexes.Add(largeIndex.Value);
+        //        return Move(grid, largeIndex.Value.Item1, largeIndex.Value.Item2, result);
+        //    }
+        //    return result; ;
+        //}
+    }
+}

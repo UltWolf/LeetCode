75b0777558dd0d9eb9938ddec91620b0b121f436
ultwolf <ultwolf@gmail.com>
2024-05-15 16:46:26 +0300

    solution

---
+++ LeetCode.Test/Common/FindWordsContaining.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Common.Tasks;
+using System.Collections;
+
+namespace LeetCode.Test.Common
+{
+    public class FindWordsContainingTest
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new string[] { "leet", "code" },
+                    'e',
+                    new List<int>() { 0, 1 }
+                    );
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TesContaining(string[] words, char x, List<int> expectedResult)
+        {
+            Assert.AreEqual(new FindWordsContainingClass().FindWordsContaining(words, x), expectedResult);
+        }
+    }
+}
---
+++ LeetCode/Common/Tasks/FindWordsContaining.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Common.Tasks
+{
+    public class FindWordsContainingClass
+    {
+
+        public IList<int> FindWordsContaining(string[] words, char x)
+        {
+            return words.Select((word, index) => new { word, index }).Where(f => f.word.Contains(x)).Select(f => f.index).ToList();
+        }
+
+        //public IList<int> FindWordsContaining(string[] words, char x)
+        //{
+        //    List<int> result = new List<int>();
+        //    for (var i = 0; i < words.Length; i++)
+        //    {
+        //        if (words[i].Any(f => f == x))
+        //        {
+        //            result.Add(i);
+        //        }
+        //    }
+        //    return result;
+        //}
+
+        //public IList<int> FindWordsContaining(string[] words, char x)
+        //{
+        //    List<int> result = new List<int>();
+        //    for (var i = 0; i < words.Length; i++)
+        //    {
+        //        if (words[i].Contains(x))
+        //        {
+        //            result.Add(i);
+        //        }
+        //    }
+        //    return result;
+        //}
+    }
+}

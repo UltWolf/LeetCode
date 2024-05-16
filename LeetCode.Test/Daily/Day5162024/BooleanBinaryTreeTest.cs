a0818b11a70f11f83df358df91eb1af79c054b9e
ultwolf <ultwolf@gmail.com>
2024-05-16 15:58:01 +0300

    Solution

---
+++ LeetCode.Test/Daily/Day5162024/BooleanBinaryTreeTest.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Common.Classes;
+using LeetCode.Daily.Day5162024;
+using System.Collections;
+
+namespace LeetCode.Test.Daily.Day5162024
+{
+    public class BooleanBinaryTreeTest
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new TreeNode() { val = 2, left = new TreeNode() { val = 1 }, right = new TreeNode() { val = 3, left = new TreeNode() { val = 0 }, right = new TreeNode() { val = 1 } } });
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TestBinaryTree(TreeNode treeNode, bool expectedResult)
+        {
+            Assert.AreEqual(new BooleanBinaryTree().EvaluateTree(treeNode), expectedResult);
+        }
+    }
+
+}
---
+++ LeetCode/Daily/Day5162024/BooleanBinaryTree.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Common.Classes;
+
+namespace LeetCode.Daily.Day5162024
+{
+    public class BooleanBinaryTree
+    {
+
+
+
+        public bool EvaluateTree(TreeNode root)
+        {
+            switch (root.val)
+            {
+                case 0:
+                    return false;
+                case 1:
+                    return true;
+                case 2:
+                    return EvaluateTree(root.left) || EvaluateTree(root.right);
+                default:
+                    return EvaluateTree(root.left) && EvaluateTree(root.right);
+            }
+        }
+
+        //public bool EvaluateTree(TreeNode root)
+        //{
+        //    if (root.val == 0)
+        //    {
+        //        return false;
+        //    }
+        //    if (root.val == 1)
+        //    {
+        //        return true;
+        //    }
+
+        //    if (root.val == 2)
+        //    {
+        //        return EvaluateTree(root.left) || EvaluateTree(root.right);
+        //    }
+        //    else
+        //    {
+        //        return EvaluateTree(root.left) && EvaluateTree(root.right);
+        //    }
+        //}
+
+    }
+}

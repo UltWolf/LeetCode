66fc46aae188f59b54ddf89d50e99397cf1ecf1f
ultwolf <ultwolf@gmail.com>
2024-05-16 17:37:36 +0300

    solution

---
+++ LeetCode/Common/Tasks/NumIdentiacalPair.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Common.Tasks
+{
+    public class NumIdentiacalPair
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            var count = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        count += 1;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}

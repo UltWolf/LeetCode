0d8e809be5332d5763395b7b1426edafcbb4fe45
ultwolf <ultwolf@gmail.com>
2024-05-17 16:08:03 +0300

    solution

---
+++ LeetCode.Test/Daily/Day5172024.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Common.Classes;
+using LeetCode.Daily.Day5172024;
+using System.Collections;
+
+namespace LeetCode.Test.Daily
+{
+    public class Day5172024
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return
+                    new TestCaseData(
+                        new TreeNode() { val = 1, left = new TreeNode() { val = 2, left = new TreeNode() { val = 2 } }, right = new TreeNode() { val = 3, left = new TreeNode() { val = 2 }, right = new TreeNode() { val = 4 } } },
+                        2,
+                        new TreeNode() { val = 1, left = null, right = new TreeNode() { val = 3, left = null, right = new TreeNode() { val = 4 } } }
+
+                    );
+                yield return
+                   new TestCaseData(
+                       new TreeNode() { val = 1, left = new TreeNode() { val = 1, }, right = new TreeNode() { val = 1 } },
+                       1,
+                       null
+
+                   );
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TestRemoveLeaves(TreeNode treeNode, int target, TreeNode expectedResult)
+        {
+            var result = new RemoveLeaves().RemoveLeafNodes(treeNode, target);
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day5172024/RemoveLeaves.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Common.Classes;
+
+namespace LeetCode.Daily.Day5172024
+{
+    public class RemoveLeaves
+    {
+        public TreeNode RemoveLeafNodes(TreeNode root, int target)
+        {
+
+            return Bfs(root, target);
+        }
+        public TreeNode Bfs(TreeNode root, int target)
+        {
+            if (root.left != null)
+            {
+                Bfs(root.left, target);
+                if (root.left.val == target && root.left.left == null && root.left.right == null)
+                {
+                    root.left = null;
+                }
+            }
+            if (root.right != null)
+            {
+                Bfs(root.right, target);
+                if (root.right.val == target && root.right.left == null && root.right.right == null)
+                {
+                    root.right = null;
+                }
+            }
+            if (root.val == target && root.right == null && root.left == null)
+            {
+                return null;
+            }
+
+            return root;
+        }
+    }
+}

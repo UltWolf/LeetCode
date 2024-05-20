138e9635c3e2fef9cfe423fec9baa4af5c9ed3a4
ultwolf <ultwolf@gmail.com>
2024-05-20 14:37:00 +0300

    solution

---
+++ LeetCode/Daily/Day5202024/SubsetXor.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Daily.Day5202024
+{
+    public class SubsetXor
+    {
+        public int SubsetXORSum(int[] nums)
+        {
+            return subsetXorSum(nums, 0, 0);
+        }
+        public int subsetXorSum(int[] nums, int index, int currentXOR)
+        {
+            if (index == nums.Length) return currentXOR;
+
+            int withElement = subsetXorSum(nums, index + 1, currentXOR ^ nums[index]);
+
+            int withoutElement = subsetXorSum(nums, index + 1, currentXOR);
+
+            return withElement + withoutElement;
+        }
+    }
+}

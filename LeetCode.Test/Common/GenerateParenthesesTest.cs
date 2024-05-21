b56f62416334b05c1cb3975aa2d596ba4dbaa77e
ultwolf <ultwolf@gmail.com>
2024-05-21 19:10:42 +0300

    solution

---
+++ LeetCode.Test/Common/GenerateParenthesesTest.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Common.Tasks;
+using System.Collections;
+
+namespace LeetCode.Test.Common
+{
+    public class GenerateParenthesesTest
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    3,
+                    new List<string>() { "((()))", "(()())", "(())()", "()(())", "()()()" });
+                yield return new TestCaseData(
+                1,
+                new List<string>() { "()" });
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TestBoats(int numberParentheses, IList<string> expectedResult)
+        {
+            Assert.AreEqual(expectedResult, new GenerateParentheses().GenerateParenthesis(numberParentheses));
+        }
+    }
+}
---
+++ LeetCode/Common/Tasks/GenerateParentheses.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Common.Tasks
+{
+    public class GenerateParentheses
+    {
+        public IList<string> output { get; set; } = new List<string>();
+        public int countParent;
+        public IList<string> GenerateParenthesis(int n)
+        {
+            countParent = n;
+            GenerateParenthesis("", 0, 0);
+            return output;
+        }
+        public void GenerateParenthesis(string result, int open, int closed)
+        {
+            if (open == countParent && closed == countParent)
+            {
+                output.Add(result);
+                return;
+            }
+            if (open < countParent)
+            {
+                GenerateParenthesis(result + "(", open + 1, closed);
+            }
+            if (closed < open)
+            {
+                GenerateParenthesis(result + ")", open, closed + 1);
+            }
+        }
+    }
+}

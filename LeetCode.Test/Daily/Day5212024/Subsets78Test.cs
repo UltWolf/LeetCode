702975f8cdf3a8c0c8a72e826d100a110e9991e5
ultwolf <ultwolf@gmail.com>
2024-05-21 12:50:32 +0300

    solution

---
+++ LeetCode.Test/Daily/Day5212024/Subsets78Test.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Daily.Day5212024;
+using System.Collections;
+
+namespace LeetCode.Test.Daily.Day5212024
+{
+    public class Subsets78Test
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 1, 2, 3 }, new List<IList<int>>() {
+                    new List<int>(),
+                    new List<int>(){ 1 },
+                    new List<int>(){ 2 },
+                    new List<int>(){ 1,2 },
+                    new List<int>(){ 3 },
+                    new List<int>(){ 1,3 },
+                    new List<int>(){ 2,3 },
+                    new List<int>(){ 1,2,3 },
+                });
+                yield return new TestCaseData(new int[] { 0 }, new List<IList<int>>() {
+                    new List<int>(),
+                    new List<int>(){0 },
+                });
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TestBoats(int[] array, IList<IList<int>> expectedResult)
+        {
+            Assert.AreEqual(expectedResult, new Subsets78().Subsets(array));
+        }
+    }
+}
---
+++ LeetCode/Daily/Day5212024/Subsets78.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCode.Daily.Day5212024
+{
+    public class Subsets78
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var result = new List<IList<int>>() { new List<int>() };
+            Subsets(nums, 0, result);
+            return result;
+        }
+
+        public void Subsets(int[] nums, int index, IList<IList<int>> result)
+        {
+            if (nums.Length == index)
+            {
+                return;
+            }
+
+            int currentResultCount = result.Count;
+            for (int i = 0; i < currentResultCount; i++)
+            {
+
+                var newSubset = new List<int>(result[i]);
+                newSubset.Add(nums[index]);
+                result.Add(newSubset);
+            }
+
+            Subsets(nums, index + 1, result);
+        }
+        //another, not mine solution
+        //private List<IList<int>> output = new List<IList<int>>();
+        //private int n, k;
+
+        //private void backtrack(int first, List<int> curr, int[] nums)
+        //{
+        //    // if the combination is done
+        //    if (curr.Count == k)
+        //        output.Add(new List<int>(curr));
+
+
+        //    for (int i = first; i < n; ++i)
+        //    {
+        //        // Add i into the current combination
+        //        curr.Add(nums[i]);
+        //        // Use next integers to complete the combination
+        //        backtrack(i + 1, curr, nums);
+        //        // backtrack
+        //        curr.RemoveAt(curr.Count - 1);
+        //    }
+        //}
+
+        //public IList<IList<int>> Subsets(int[] nums)
+        //{
+        //    n = nums.Length;
+        //    for (k = 0; k < n + 1; ++k)
+        //    {
+        //        backtrack(0, new List<int>(), nums);
+        //    }
+
+        //    return output;
+        //}
+    }
+}

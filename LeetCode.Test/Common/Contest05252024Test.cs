54743f0a7221a225a429336208c3490747146d94
ultwolf <ultwolf@gmail.com>
2024-05-25 20:07:34 +0300

    solution

---
+++ LeetCode.Test/Common/Contest05252024Test.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Common.Contest;
+using System.Collections;
+
+namespace LeetCode.Test.Common
+{
+    public class Contest05252024Test
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 1, 2, 1, 3 }, 1);
+                yield return new TestCaseData(new int[] { 1, 2, 3 }, 0);
+                yield return new TestCaseData(new int[] { 1, 2, 2, 1 }, 3);
+                yield return new TestCaseData(new int[] { 1, 1, 2, 2, 3, 3, 4, 4, 5 }, 5);
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void TesContaining(int[] nums, int expectedResult)
+        {
+            Assert.AreEqual(new Contest05252024().DuplicateNumbersXOR(nums), expectedResult);
+        }
+    }
+}
---
+++ LeetCode/Common/Contest/Contest05252024.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode.Common.Contest
+{
+    public class Contest05252024
+    {
+
+        //public int DuplicateNumbersXOR(int[] nums)
+        //{
+        //    Dictionary<int, int> count = new Dictionary<int, int>();
+
+
+        //    foreach (int num in nums)
+        //    {
+        //        if (count.ContainsKey(num))
+        //        {
+        //            count[num]++;
+        //        }
+        //        else
+        //        {
+        //            count[num] = 1;
+        //        }
+        //    }
+
+        //    int xorResult = 0;
+
+        //    foreach (var entry in count)
+        //    {
+        //        if (entry.Value == 2)
+        //        {
+        //            xorResult ^= entry.Key;
+        //        }
+        //    }
+
+        //    return xorResult;
+        //}
+
+        public IList<bool> GetResults(int[][] queries)
+        {
+            var results = new List<bool>();
+
+            return results;
+        }
+        public int DuplicateNumbersXOR(int[] nums)
+        {
+
+            List<int> result = new List<int>();
+            Array.Sort(nums);
+            for (var i = nums.Length - 1; i > 0; i--)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    result.Add(nums[i]);
+                }
+            }
+            if (result.Count > 0)
+            {
+                var output = result.ElementAt(0);
+                for (var i = 1; i < result.Count; i++)
+                {
+                    output ^= result[i];
+                }
+                return output;
+            }
+            else
+            {
+                return 0;
+
+            }
+        }
+    }
+}

68deed5180b03aaaeefce9808eac8f2025ec83a0
ultwolf <ultwolf@gmail.com>
2024-05-28 11:33:50 +0300

    solution

---
+++ LeetCode.Test/Daily/Day05282024/SubBudgetTest.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Daily.Day05282024;
+using System.Collections;
+
+namespace LeetCode.Test.Daily.Day05282024
+{
+    public class SubBudgetTest
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("abcd", "bcdf", 3).Returns(3);
+                yield return new TestCaseData("abcd", "cdef", 3).Returns(1);
+                yield return new TestCaseData("abcd", "acde", 0).Returns(1);
+                yield return new TestCaseData("abcd", "cdef", 1).Returns(0);
+                yield return new TestCaseData("pxezla", "loewbi", 25).Returns(4);
+                yield return new TestCaseData("krrgw", "zjxss", 19).Returns(2);
+                yield return new TestCaseData("abcd", "abcd", 10).Returns(4);
+                yield return new TestCaseData("abcdef", "azcdef", 1).Returns(1);
+                yield return new TestCaseData("xyz", "abc", 10).Returns(0);
+                yield return new TestCaseData("kpm", "kpn", 1).Returns(2);
+                yield return new TestCaseData("abcdxyz", "abcfxyz", 2).Returns(4);
+                yield return new TestCaseData("wxyz", "xyzw", 100).Returns(4);
+                yield return new TestCaseData("aaaaaaaa", "bbbbbbbb", 8).Returns(4);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public int TestEqualSubstring(string s, string t, int maxCost)
+        {
+            return new SubBudget().EqualSubstring(s, t, maxCost);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day05282024/SubBudget.cs
@@ -0,0 +1,85 @@
+﻿namespace LeetCode.Daily.Day05282024
+{
+    public class SubBudget
+    {
+        public int EqualSubstring(string s, string t, int maxCost)
+        {
+            int maxLength = 0;
+            int currentCost = 0;
+            int start = 0;
+
+            for (int end = 0; end < s.Length; end++)
+            {
+                currentCost += Math.Abs(s[end] - t[end]);
+
+                while (currentCost > maxCost)
+                {
+                    currentCost -= Math.Abs(s[start] - t[start]);
+                    start++;
+                }
+
+                maxLength = Math.Max(maxLength, end - start + 1);
+            }
+
+            return maxLength;
+            //int maxLength = 0;
+            //for (var i = 0; i < s.Length; i++)
+            //{
+
+            //    if (s[i] == t[i])
+            //    {
+            //        maxLength++;
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        var a = (int)s[i];
+            //        var b = (int)t[i];
+            //        var sum = Math.Abs(a - b);
+            //        if (sum <= maxCost)
+            //        {
+            //            maxCost -= sum;
+            //            maxLength++;
+            //            //if (maxCost == 0)
+            //            //{
+            //            //    break;
+            //            //}
+            //        }
+
+            //    }
+
+            //}
+            //return maxLength;
+            //int maxLength = 0;
+            //int[] sLength = new int[s.Length];
+            //for (var i = 0; i < s.Length; i++)
+            //{
+
+            //    var a = (int)s[i];
+            //    var b = (int)t[i];
+            //    var sum = Math.Abs(a - b);
+            //    sLength[i] = sum;
+
+            //}
+            //Array.Sort(sLength);
+            //int j = 0;
+            //while (j < s.Length)
+            //{
+            //    var sum = maxCost - sLength[j];
+            //    if (sum >= 0)
+            //    {
+            //        maxCost = sum;
+            //        maxLength++; j++;
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+
+            //}
+            //return maxLength;
+
+        }
+
+    }
+}

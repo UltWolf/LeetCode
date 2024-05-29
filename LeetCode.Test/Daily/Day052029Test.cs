fa9cd8d77e5841d4f6b59a8fb20be0d1a717619a
ultwolf <ultwolf@gmail.com>
2024-05-29 12:40:36 +0300

    solution

---
+++ LeetCode.Test/Daily/Day052029Test.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Daily.Day05292024;
+using System.Collections;
+
+namespace LeetCode.Test.Daily
+{
+    public class Day052029Test
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("1101").Returns(6);
+                yield return new TestCaseData("10").Returns(1);
+                yield return new TestCaseData("1").Returns(0);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public int TestEqualSubstring(string s)
+        {
+            return new ReduceBinaryTo1().NumSteps(s);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day05292024/ReduceBinaryTo1.cs
@@ -0,0 +1,74 @@
+﻿namespace LeetCode.Daily.Day05292024
+{
+    public class ReduceBinaryTo1
+    { //var number = Convert.ToInt32(s, 2);
+      //var result = 0;
+      //while (number > 1)
+      //{
+      //    if (number % 2 == 0)
+      //    {
+      //        number /= 2;
+      //    }
+      //    else
+      //    {
+      //        number++;
+      //    }
+      //    result++;
+      //}
+      //return result;
+        public int NumSteps(string s)
+        {
+            var result = 0;
+            while (s != "1")
+            {
+
+
+                if (s[s.Length - 1] == '0')
+                {
+                    s = BinaryDivideBy2(s);
+                }
+                else
+                {
+                    s = SumBinary(s.ToCharArray());
+                }
+                result++;
+            }
+            return result;
+        }
+        //110 = 011
+        //010 = 001
+        //100 = 010
+        public string BinaryDivideBy2(string s)
+        {
+            return s.Substring(0, s.Length - 1);
+        }
+        //011 = 100
+        //001 = 010
+        //101 = 110
+
+        public string SumBinary(char[] s)
+        {
+            int accumulate = 1;
+            s[s.Length - 1] = '0';
+            for (var i = s.Length - 2; i >= 0 && accumulate >= 1; i--)
+            {
+                accumulate--;
+                if (s[i] == '0')
+                {
+                    s[i] = '1';
+                }
+                else
+                {
+                    s[i] = '0';
+                    accumulate++;
+                }
+            }
+            var result = new string(s);
+            if (result[0] == '0')
+            {
+                result = '1' + result;
+            }
+            return result;
+        }
+    }
+}

22a1266bf1a1d5a090f86584bf3b35c314ec89fb
ultwolf <ultwolf@gmail.com>
2024-05-31 16:51:30 +0300

    solution

---
+++ LeetCode.Test/Daily/Day20240531Test.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Daily;
+using System.Collections;
+
+namespace LeetCode.Test.Daily
+{
+    public class Day20240531Test
+    {
+        private static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new int[] { 1, 2, 3, 1, 2, 5 }
+                    ).Returns(new int[] { 3, 5 });
+            }
+        }
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public int[] Test(int[] n)
+        {
+            return new Day20240531().SingleNumber(n);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day20240531.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode.Daily
+{
+    //    for (int i = 0; i < nums.Length; i++)
+    //            {
+    //                bool isTwice = false;
+    //                for (int j = i + 1; j<nums.Length; j++)
+    //                {
+    //                    if (nums[j] == nums[i])
+    //                    {
+    //                        isTwice = true;
+    //                        break;
+    //                    }
+    //                }
+    //                if (!isTwice)
+    //{
+    //    numbers.Add(nums[i]);
+    //}
+    //            }
+    //            return numbers.ToArray();
+    public class Day20240531
+    {
+        List<int> numbers = new List<int>();
+        public int[] SingleNumber(int[] nums)
+        {
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int n = nums[i];
+                if (!dict.ContainsKey(n)) { dict[n] = 1; }
+                else { dict[n]++; }
+            }
+
+            foreach (int n in dict.Keys)
+            {
+                if (dict[n] == 1) { numbers.Add(n); }
+            }
+
+            return numbers.ToArray();
+            //Array.Sort(nums);
+            //for (var i = 0; i < nums.Length;)
+            //{
+            //    var newIndex = i + 1;
+            //    if (newIndex < nums.Length && nums[i] == nums[newIndex])
+            //    {
+            //        i += 2;
+            //    }
+            //    else
+            //    {
+            //        numbers.Add(nums[i]);
+            //        i++;
+            //    }
+            //}
+            //return numbers.ToArray();
+
+            //for (var i = 0; i < nums.Length; i++)
+            //{
+            //    if (numbers.Any(f => f == nums[i]))
+            //    {
+            //        numbers.Remove(nums[i]);
+            //    }
+            //    else
+            //    {
+            //        numbers.Add(nums[i]);
+            //    }
+            //}
+            //return numbers.ToArray();
+        }
+    }
+}

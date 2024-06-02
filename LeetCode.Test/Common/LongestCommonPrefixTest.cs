18d39d902ddaaf28cf5f6d371a91725f0bc21b33
ultwolf <ultwolf@gmail.com>
2024-06-02 16:59:05 +0300

    sol

---
+++ LeetCode.Test/Common/LongestCommonPrefixTest.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Common.Tasks;
+
+namespace LeetCode.Test.Common
+{
+    public class LongestCommonPrefixTest
+    {
+        public static IEnumerable<TestCaseData> TestCase
+        {
+            get
+            {
+                yield return new TestCaseData("fl", new string[] { "flower", "flow", "flight" }) { TestName = "test1" }.Returns("fl");
+                yield return new TestCaseData("", new string[] { "dog", "racecar", "car" }) { TestName = "test2" }.Returns("");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCase))]
+        public string LongestCommonPrefix(string result, string[] strs)
+        {
+            return new LongestCommonPrefixWrapper().LongestCommonPrefix(strs);
+        }
+    }
+}
---
+++ LeetCode/Common/Tasks/LongestCommonPrefix.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace LeetCode.Common.Tasks
+{
+    public class LongestCommonPrefixWrapper
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Array.Sort(strs, (str1, str2) =>
+            {
+
+                if (str1.Length == str2.Length)
+                {
+                    return 0;
+                }
+                return str1.Length <= str2.Length ? -1 : 1;
+            });
+            var length = strs[0].Length;
+            var strLength = strs.Length - 1;
+            for (var j = 0; j < length; j++)
+            {
+                var isEqual = true;
+                for (var i = 0; i < strLength; i++)
+                {
+
+                    if (strs[i][j] != strs[i + 1][j])
+                    {
+                        isEqual = false;
+                    }
+                }
+                if (isEqual)
+                {
+                    stringBuilder.Append(strs[0][j]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}

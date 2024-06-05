67bbc8942ccc2d3288b3cbc721ef2de6d21c6813
ultwolf <ultwolf@gmail.com>
2024-06-05 19:03:41 +0300

    solution

---
+++ LeetCode.Test/Daily/Day20240605Test.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Daily;
+using System.Collections;
+
+namespace LeetCode.Test.Daily
+{
+    public class Day20240605Test
+    {
+        public static IEnumerable TestCase
+        {
+            get
+            {
+                yield return new TestCaseData("", new string[] { "bella", "label", "roller" }) { TestName = "test" }.Returns(new string[] { "e", "l", "l" });
+                yield return new TestCaseData("", new string[] { "cool", "lock", "cook" }) { TestName = "test2" }.Returns(new string[] { "c", "o" });
+            }
+        }
+        [Test, TestCaseSource(nameof(TestCase))]
+        public string[] CheckDuplicates(string s, string[] array)
+        {
+            return new Day20240605().Calculate(array).ToArray();
+        }
+
+    }
+}
---
+++ LeetCode/Daily/Day20240605.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Daily
+{
+    public class Day20240605
+    {
+        //        Input: words = ["bella","label","roller"]
+        //        Output: ["e","l","l"]
+        //        Example 2:
+
+        //Input: words = ["cool","lock","cook"]
+        //        Output: ["c","o"]
+        public List<string> Calculate(string[] array)
+        {
+            Array.Sort(array);
+            List<string> list = new List<string>();
+            for (var s = 0; s < array[0].Length; s++)
+            {
+                var letter = array[0][s];
+
+                var isFind = false;
+                for (var i = 1; i < array.Length; i++)
+                {
+                    isFind = false;
+                    for (var j = 0; j < array[i].Length; j++)
+                    {
+                        if (array[i][j] == letter)
+                        {
+                            isFind = true;
+                            array[i] = array[i].Remove(j, 1);
+                            break;
+                        }
+                    }
+                    if (isFind == false)
+                    {
+                        break;
+                    }
+                }
+                if (isFind != false)
+                {
+                    list.Add(letter.ToString());
+                }
+            }
+            return list;
+        }
+    }
+}

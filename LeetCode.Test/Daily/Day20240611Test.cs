a75462b2ac0ee1007e97d0934f4c315830d8c29b
ultwolf <ultwolf@gmail.com>
2024-06-11 20:50:17 +0300

    some sol

---
+++ LeetCode.Test/Daily/Day20240611Test.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Daily;
+using System.Collections;
+
+namespace LeetCode.Test.Daily
+{
+    public class Day20240611Test
+    {
+        public static IEnumerable TestCase
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19 }, new int[] { 2, 1, 4, 3, 9, 6 }).Returns(new int[] { 2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19 });
+            }
+        }
+        [Test, TestCaseSource(nameof(TestCase))]
+        public int[] Sort(int[] arr1, int[] arr2)
+        {
+            return new Day20240611().RelativeSortArray(arr1, arr2);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day20240611.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Daily
+{
+    public class Day20240611
+    {
+        public int[] RelativeSortArray(int[] arr1, int[] arr2)
+        {
+            int[] result = new int[arr1.Length];
+            int index = 0;
+            Array.Sort(arr1);
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (var j = 0; j < arr1.Length; j++)
+                {
+                    if (arr2[i] == arr1[j])
+                    {
+                        result[index++] = arr1[j];
+                    }
+                }
+            }
+            for (var i = index; i < result.Length; i++)
+            {
+                result[i] = arr1[i];
+            }
+            return result;
+        }
+    }
+}

6d211166af5fbfcae345a9fe29b71fe02c36597b
ultwolf <ultwolf@gmail.com>
2024-06-13 16:57:45 +0300

    sol

---
+++ LeetCode/Daily/Day20240613.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Daily
+{
+    public class Day20240613
+    {
+        public int MinMovesToSeat(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+            var result = 0;
+            for (var i = 0; i < seats.Length; i++)
+            {
+                result += Math.Abs(seats[i] - students[i]);
+            }
+            return result;
+        }
+    }
+}

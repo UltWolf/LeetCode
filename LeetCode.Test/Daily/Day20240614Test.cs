aa6a94cf62a371811f178236394f88f7e2deba9b
ultwolf <ultwolf@gmail.com>
2024-06-14 19:23:01 +0300

    sol

---
+++ LeetCode.Test/Daily/Day20240614Test.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Daily;
+using System.Collections;
+
+namespace LeetCode.Test.Daily
+{
+    public class Day20240614Test
+    {
+        public static IEnumerable TestCase
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 1, 2, 2 }).Returns(1);
+                yield return new TestCaseData(new int[] { 3, 2, 1, 2, 1, 7 }).Returns(6);
+                yield return new TestCaseData(new int[] { 1, 1, 1, 1, 1 }).Returns(10);
+            }
+        }
+        [Test, TestCaseSource(nameof(TestCase))]
+        public int TestArraySort(int[] array)
+        {
+            return new Day20240614().MinIncrementForUnique(array);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day20240614.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Daily
+{
+    public class Day20240614
+    {
+        public int MinIncrementForUnique(int[] nums)
+        {
+            Array.Sort(nums);
+            int result = 0;
+            int previos = nums[0];
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var curr = nums[i];
+                if (curr > previos)
+                {
+                    previos = curr;
+                    continue;
+                }
+                var diff = (curr - previos) * (-1) + 1;
+                if (diff > 0)
+                {
+                    curr += diff;
+                    result += diff;
+                }
+                //while (curr <= previos)
+                //{
+                //    curr += 1;
+                //    result++;
+                //}
+                nums[i] = curr;
+                previos = curr;
+            }
+            return result;
+        }
+    }
+}

968de7da2423bc483da3e0a9708741e98b6cc570
ultwolf <ultwolf@gmail.com>
2024-06-18 15:09:11 +0300

    push

---
+++ LeetCode.Test/Daily/Day20240618Test.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Daily;
+using System.Collections;
+
+namespace LeetCode.Test.Daily
+{
+    public class Day20240618Test
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 2, 4, 6, 8, 10 }, new int[] { 10, 20, 30, 40, 50 }, new int[] { 4, 5, 6, 7 }).Returns(100);
+                yield return new TestCaseData(new int[] { 68, 35, 52, 47, 86 }, new int[] { 67, 17, 1, 81, 3 }, new int[] { 92, 10, 85, 84, 82 }).Returns(324);
+            }
+
+        }
+        [Test, TestCaseSource(nameof(TestCases))]
+        public int Test(int[] dif, int[] worker, int[] workers)
+        {
+            return new Day20240618().MaxProfitAssignment(dif, worker, workers);
+        }
+    }
+}
---
+++ LeetCode/Daily/Day20240618.cs
@@ -0,0 +1,88 @@
+﻿namespace LeetCode.Daily
+{
+    public class Day20240618
+    {
+        //public class ProfitDif
+        //{
+        //    public int difficulty;
+        //    public int profit;
+
+        //}
+        //public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker)
+        //{
+        //    ProfitDif[] work = new ProfitDif[difficulty.Length];
+        //    for (var i = 0; i < difficulty.Length; i++)
+        //    {
+        //        work[i] = new ProfitDif() { difficulty = difficulty[i], profit = profit[i] };
+        //    }
+        //    var maxValue = 0;
+        //    Array.Sort(work, (item1, item2) => { return item1.difficulty.CompareTo(item2.difficulty); });
+        //    for (var i = 0; i < worker.Length; i++)
+        //    {
+        //        var prof = 0;
+        //        for (var j = 0; j < work.Length; j++)
+        //        {
+        //            if (work[j].difficulty > worker[i])
+        //            {
+        //                break;
+        //            }
+        //            else if (work[j].profit > prof)
+        //            {
+        //                prof = work[j].profit;
+        //            }
+        //        }
+        //        maxValue += prof;
+        //    }
+        //    return maxValue;
+        //}
+
+        //public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker)
+        //{
+        //    Array.Sort(difficulty, profit);  
+        //    int maxValue = 0;
+        //    for (var i = 0; i < worker.Length; i++)
+        //    {
+        //        var prof = 0;
+        //        for (var j = 0; j < difficulty.Length; j++)
+        //        {
+        //            if (difficulty[j] > worker[i])
+        //            {
+        //                break;
+        //            }
+        //            else if (profit[j] > prof)
+        //            {
+        //                prof = profit[j];
+        //            }
+        //        }
+        //        maxValue += prof;
+        //    }
+        //    return maxValue;
+        //}
+        public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker)
+        {
+            Array.Sort(difficulty, profit);
+            Array.Sort(worker);
+            int maxValue = 0;
+            int i = 0;
+            int j = 0;
+            var prof = 0;
+            while (i < worker.Length)
+            {
+
+                if (j < difficulty.Length && worker[i] >= difficulty[j])
+                {
+                    prof = Math.Max(prof, profit[j]);
+                    j++;
+                }
+                else
+                {
+                    i++;
+                    maxValue += prof;
+                }
+
+            }
+
+            return maxValue;
+        }
+    }
+}
